3e4829b79f35d102280c4f812b34d4e23342d4c8
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-06-19 09:08:38 +0700

    Initial Commit

---
+++ Cendekiawan/CendekiawanApp.swift
@@ -0,0 +1,17 @@
+//
+//  CendekiawanApp.swift
+//  Cendekiawan
+//
+//  Created by Hans Arthur Cupiterson on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CendekiawanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
