8a1c1e92ea80b32e9cf36b96eb534235cbbacdfe
NJuGau <nathanael.gauthama@binus.ac.id>
2024-06-19 11:21:10 +0700

    Create Quiz View

---
+++ Cendekiawan/QuizView.swift
@@ -0,0 +1,18 @@
+//
+//  QuizView.swift
+//  Cendekiawan
+//
+//  Created by Nathanael Juan Gauthama on 19/06/24.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    QuizView()
+}
