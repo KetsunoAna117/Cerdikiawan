fe795869d4d2e4233e578aed8ccec400887cff6f
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-06-20 09:30:46 +0700

    Add Model data & connect to firebase

---
+++ Cendekiawan/Model/Choice.swift
@@ -0,0 +1,18 @@
+//
+//  Choice.swift
+//  Cendekiawan
+//
+//  Created by Bryan Vernanda on 19/06/24.
+//
+
+import Foundation
+
+class Choice {
+    var choiceText: String
+    var choiceID: Int
+    
+    init(choiceText: String, choiceID: Int) {
+        self.choiceText = choiceText
+        self.choiceID = choiceID
+    }
+}
