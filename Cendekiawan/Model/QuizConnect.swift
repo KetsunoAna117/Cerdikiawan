5f48de20ba6076fd3a4a709a10491caa418d1382
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-06-21 09:14:30 +0700

    Updated Logic for Quiz Connect

--- Cendekiawan/Model/QuizConnect.swift
+++ Cendekiawan/Model/QuizConnect.swift
@@ -11,13 +11,11 @@ class QuizConnect: Quiz {
     var quizPrompt: String
     var quizLeftChoiceList: [Choice]
     var quizRightChoiceList: [Choice]
-    var quizAnswerList: [(Int, Int)]
     
-    init(quizId: Int, quizFeedback: Feedback, quizDifficultyLevel: Int, quizCategory: String, quizTitle: String, quizAsset: [String], isRedemption: Bool, quizPrompt: String, quizLeftChoiceList: [Choice], quizRightChoiceList: [Choice], quizAnswerList: [(Int, Int)]) {
+    init(quizId: Int, quizFeedback: Feedback, quizDifficultyLevel: Int, quizCategory: String, quizTitle: String, quizAsset: [String], isRedemption: Bool, quizPrompt: String, quizLeftChoiceList: [Choice], quizRightChoiceList: [Choice]) {
         self.quizPrompt = quizPrompt
         self.quizLeftChoiceList = quizLeftChoiceList
         self.quizRightChoiceList = quizRightChoiceList
-        self.quizAnswerList = quizAnswerList
         
         super.init(quizId: quizId, quizFeedback: quizFeedback, quizDifficultyLevel: quizDifficultyLevel, quizCategory: quizCategory, quizTitle: quizTitle, quizAsset: quizAsset, isRedemption: isRedemption)
     }
