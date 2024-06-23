f34d59f78e1d33d897928e470e9d4148e3ab7043
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-06-23 15:56:16 +0700

    Refactor QuizFillBlankView
    Moved QuizFillBlankView Business Logic into a viewmodel

--- Cendekiawan/QuizFillBlankView.swift
+++ Cendekiawan/QuizFillBlankView.swift
@@ -8,26 +8,15 @@
 import SwiftUI
 
 struct QuizFillBlankView: View {
-    @State var questions = "Lorem Ipsum dolor sit amet lorem ipsum dolor sit amet Lorem Ipsum dolor sit amet lorem ipsum Lorem __________ sit amet lorem ipsum dolor sit amet Lorem Ipsum dolor sit amet lorem ipsum Lorem Ipsum dolor sit amet lorem ipsum dolor sit amet Lorem Ipsum dolor sit amet lorem __________ Ipsum dolor sit amet lorem ipsum dolor sit amet Lorem Ipsum dolor sit amet lorem __________ Ipsum dolor sit amet lorem ipsum dolor sit amet Lorem Ipsum dolor sit amet lorem ipsum Lorem Ipsum dolor sit amet lorem ipsum dolor sit amet Lorem Ipsum dolor sit amet lorem ipsum Lorem Ipsum dolor sit amet lorem __________ sit amet Lorem Ipsum dolor sit amet lorem ipsum Lorem Ipsum dolor sit amet"
+    @ObservedObject var vm: QuizFillBlankViewModel
     
-    @State private var choices: [DraggableChoice] = [
-        DraggableChoice(choiceID: 1, choiceText: "jawaban A"),
-        DraggableChoice(choiceID: 2, choiceText: "jawaban B"),
-        DraggableChoice(choiceID: 3, choiceText: "jawaban C"),
-        DraggableChoice(choiceID: 4, choiceText: "jawaban D")
-    ]
-    @State private var droppedAnswer: [DraggableChoice] = []
-    
-    var trunc: [String] = []
-    
-    init() {
-        trunc = questions.split(separator: "__________").map { String($0) }
-//        for _ in 0..<(trunc.count - 1) {
-//            print("added")
-//            droppedAnswer.append(DraggableChoice(choiceID: 0, choiceText: ""))
-//            print(droppedAnswer)
-//        }
-    }
+//    init() {
+////        for _ in 0..<(trunc.count - 1) {
+////            print("added")
+////            droppedAnswer.append(DraggableChoice(choiceID: 0, choiceText: ""))
+////            print(droppedAnswer)
+////        }
+//    }
     
     var body: some View {
         HStack{
@@ -42,29 +31,19 @@ struct QuizFillBlankView: View {
                         .padding([.bottom], 30)
                     Spacer()
                     VStack(alignment: .leading) {
-                        ForEach (0..<trunc.count, id: \.self) { index in
+                        ForEach (0..<vm.trunc.count, id: \.self) { index in
                             HStack(alignment: .center){
-                                Text(trunc[index])
-                                if index < trunc.count - 1{
+                                Text(vm.trunc[index])
+                                if index < vm.trunc.count - 1{
                                     Spacer()
-                                    DroppableBox(boxText: droppedAnswer.isEmpty ? "" : droppedAnswer[index].choiceText)
+                                    DroppableBox(boxText: vm.droppedAnswer.isEmpty ? "" : vm.droppedAnswer[index].choiceText)
                                         .dropDestination(for: DraggableChoice.self) { droppedChoice, location in
                                             print(index)
-                                            let previouslySelectedChoice = droppedAnswer[index]
-                                            droppedAnswer[index] = droppedChoice[0]
-                                            choices.removeAll { $0.uniqueId == droppedChoice[0].uniqueId }
-                                            if previouslySelectedChoice.choiceID != -1 {
-                                                print(previouslySelectedChoice.choiceID)
-                                                choices.append(previouslySelectedChoice)
-                                            }
+                                            vm.handleChoiceDrop(index: index, droppedChoice: droppedChoice)
                                             return true
                                         }
                                         .onTapGesture {
-                                            // remove choices
-                                            if droppedAnswer[index].choiceID != -1 {
-                                                choices.append(droppedAnswer[index])
-                                                droppedAnswer[index] = DraggableChoice(choiceID: -1, choiceText: "")
-                                            }
+                                            vm.removeChoicesFromAnswer(index: index)
                                         }
                                 }
                                 
@@ -80,20 +59,28 @@ struct QuizFillBlankView: View {
                 Text("Masukkan kata yang tepat untuk setiap kata yang rumpang dalam teks!")
                     .font(.title3)
                     .fontWeight(.bold)
-                ChoicePoolView(choices: choices)
+                ChoicePoolView(choices: vm.choices)
             }
             .frame(maxWidth: .infinity)
         }
         .padding(50)
         .onAppear {
-            if droppedAnswer.isEmpty {
-                droppedAnswer = Array(repeating: DraggableChoice(choiceID: -1, choiceText: ""), count: trunc.count - 1)
-            }
+            vm.setupQuestion()
         }
         
     }
 }
 
 #Preview {
-    QuizFillBlankView()
+    QuizFillBlankView(
+        vm: QuizFillBlankViewModel(
+            questions: "Apa yang dicari orang __________? Bintang __________. Kita Tanpa itu, kita mempunyai __________ dan tidak akan mendapatkan __________",
+            choices: [
+                Choice(choiceID: 1, choiceText: "Sigma"),
+                Choice(choiceID: 2, choiceText: "Skibidi"),
+                Choice(choiceID: 3, choiceText: "L Rizz"),
+                Choice(choiceID: 4, choiceText: "Gyatt")
+            ]
+        )
+    )
 }
