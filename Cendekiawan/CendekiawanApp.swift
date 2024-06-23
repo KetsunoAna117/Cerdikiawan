64dfe3e2052816d26e346433f3754d72662dfe7c
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-06-23 14:57:01 +0700

    Remove Firebase Package Dependency

--- Cendekiawan/CendekiawanApp.swift
+++ Cendekiawan/CendekiawanApp.swift
@@ -6,15 +6,10 @@
 //
 
 import SwiftUI
-import Firebase
 
 @main
 struct CendekiawanApp: App {
 //    @StateObject var databaseHelper = DatabaseHelper()
-    
-    init() {
-        FirebaseApp.configure() //combine with the info.plist of GoogleService
-    }
     
     var body: some Scene {
         WindowGroup {
