650f43122edf5951c51d43efb3cb9b2eb8db5c4e
QuangHaryanto <apple.haryanto@gmail.com>
2024-06-25 18:02:34 +0700

    Code Review

--- Cendekiawan/QuizFillBlankView.swift
+++ Cendekiawan/QuizFillBlankView.swift
@@ -21,7 +21,10 @@ struct QuizFillBlankView: View {
     var body: some View {
         NavigationStack {
             VStack {
+                // TODO: reminder to remove all the test views and comments
                 StatsOverlay() // For debugging, can be removed later
+                
+                // TODO: Increase readability: for all the view inside the vstack that involved a various of stacks, consider to create a function that represent each (function of UI) view. Create an identifiable function name and let the function recieve parameters (a data or vm that holds the information the view needs to decide its behavior). Let the padding and all view positioning value to be a configurable variable consistently maintained as a single source.
                 HStack {
                     ScrollView {
                         VStack {
