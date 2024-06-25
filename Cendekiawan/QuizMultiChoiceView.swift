a8ce34d3eb1e3b6ca1d703317ff4a980b56ae907
NJuGau <nathanael.gauthama@binus.ac.id>
2024-06-26 06:43:31 +0700

    Fix answerButton UI

--- Cendekiawan/QuizMultiChoiceView.swift
+++ Cendekiawan/QuizMultiChoiceView.swift
@@ -26,12 +26,7 @@ struct QuizMultiChoiceView: View {
     @State var selectedAnswer: Int?
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                // TODO: reminder to remove all the test views and comments
-                // buat checking aja. nanti dihapus
-                StatsOverlay()
-                HStack {
+        HStack(alignment: .top) {
                     // TODO: Increase readability: for all the view inside the vstack that involved a various of stacks, consider to create a function that represent each (function of UI) view. Create an identifiable function name and let the function recieve parameters (a data or vm that holds the information the view needs to decide its behavior). Let the padding and all view positioning value to be a configurable variable consistently maintained as a single source.
                     // aku ganti jadi scrollview
                     ScrollView {
@@ -48,10 +43,13 @@ struct QuizMultiChoiceView: View {
                     .frame(maxWidth: .infinity)
                     Spacer(minLength: 20)
                     VStack(alignment: .leading, spacing: 20) {
+                        Text("Pertanyaan")
+                            .font(.headline)
+                            .fontWeight(.bold)
                         Text(quiz.quizQuestion)
                         
                         ForEach(quiz.quizChoiceList, id: \.choiceId) { choice in
-                            AnswerButton(isClicked: choice.choiceId == selectedAnswer, choice: Choice(choiceId: choice.choiceId, choiceDescription: choice.choiceDescription))
+                            AnswerButton(boxColor: Color.cerdikiawanWhite, choice: Choice(choiceId: choice.choiceId, choiceDescription: choice.choiceDescription))
                                 .onTapGesture {
                                     selectedAnswer = choice.choiceId
                                 }
@@ -60,41 +58,6 @@ struct QuizMultiChoiceView: View {
                     .frame(maxWidth: .infinity)
                 }
                 .padding(50)
-                HStack {
-                    Button {
-                        // print(user.difficultyLevel)
-                        if tipeQuiz == "idePokok" {
-                            updateIdePokokProeficiency(user: user, win: true)
-                        } else {
-                            updateImplisitProeficiency(user: user, win: true)
-                        }
-                    } label: {
-                        Text("Benar")
-                            .font(.system(size: 50))
-                            .foregroundStyle(.white)
-                            .padding()
-                            .background(.green)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
-                    Button {
-                        // print(user.difficultyLevel)
-                        if tipeQuiz == "idePokok" {
-                            updateIdePokokProeficiency(user: user, win: false)
-                        } else {
-                            updateImplisitProeficiency(user: user, win: false)
-                        }
-                    } label: {
-                        Text("Salah")
-                            .font(.system(size: 50))
-                            .foregroundStyle(.white)
-                            .padding()
-                            .background(.red)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
-                }
-            }
-
-        }
     }
     
 }
