172d288bc2d3c6d8472c1b76fe5ea3fbfa9da176
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-06-26 11:24:04 +0700

    Fix merge conflict

--- Cendekiawan/ContentView.swift
+++ Cendekiawan/ContentView.swift
@@ -11,28 +11,8 @@ struct ContentView: View {
     @Environment(QuizModelData.self) private var modelData
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                QuizFillBlankView(
-                    vm: QuizFillBlankViewModel(
-                        quizFillBlankModel: modelData.rumpang4[0]
-                    )
-                )
-                .preferredColorScheme(.light)
-                    .environment(QuizModelData())
-//                Button(action: {
-//                    vm.valueProgressBar = 0
-//                    isDirected = true
-//                    vm.startGameplay()
-//                }, label: {
-//                    Text("Play")
-//                })
-            }
-//            .navigationDestination(isPresented: $isDirected) {
-//                QuizView(vm: vm)
-//                    .environment(QuizModelData())
-//            }
-        }
+        HomeView()
+            .environment(modelData)
     }
 }
 
